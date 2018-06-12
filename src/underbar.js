1ca2fa0005ed96218abea79ca991b3dc618bb9d7
shripriyaprabhakar <shripriyaprabhakar@gmail.com>
2018-06-12 12:34:33 -0700

    Working on map

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,8 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length - 1] 
+      : array.slice(array.length - Math.min(n, array.length));
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +48,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }  
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +78,36 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+    for (var i = 0; i < collection.length; i++){
+      if (test(collection[i])) {
+        results.push(collection[i]);
+      }
+    }
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(elem) {
+      return !test(elem);
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    iterator = iterator || _.identity;
+    var results =[];
+    var seen = {};
+    for (var i = 0; i < array.length; i++){
+      if (!seen.hasOwnProperty(iterator(array[i]).toString())) {
+        seen[iterator(array[i])] = array[i];
+        results.push(array[i]);
+      }    
+    }
+    return results;
   };
 
 
